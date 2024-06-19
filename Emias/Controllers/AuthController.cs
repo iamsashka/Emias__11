dffc919fb9d94b6a715d27cfa9659983dc1fc025
ksen <kspankova13@gmail.com>
2024-06-19 20:39:57 +0300

    интерфейс авторизации доктора

---
+++ Emias/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EMIAS.Models;
+using System.Linq;
+
+namespace EMIAS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly EmiasContext _context;
+
+        public AuthController(EmiasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            var doctor = _context.Doctors
+                .FirstOrDefault(d => d.IdDoctor.ToString() == request.EmployeeNumber && d.EnterPassword == request.Password);
+
+            if (doctor == null)
+            {
+                return Unauthorized(new { Message = "Неверный номер сотрудника или пароль" });
+            }
+
+            return Ok(new { Message = "Авторизация успешна", Role = "Doctor" });
+        }
+    }
+}
---
+++ Emias/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EMIAS.Models
+{
+    public class LoginRequest
+    {
+        public string EmployeeNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
