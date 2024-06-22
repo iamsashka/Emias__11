979f7a18e829001d25de3012636d32eec2a74995
Yana Belinskaya <belikbelka@icloud.com>
2024-06-22 20:53:21 +0300

    пациент и доктор

--- Emias/Models/EmiasContext.cs
+++ Emias/Models/EmiasContext.cs
@@ -35,9 +35,6 @@ public partial class EmiasContext : DbContext
 
     public virtual DbSet<StatusType> StatusTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-6435JQ5\\SQLEXPRESS;Initial Catalog=EMIAS;Integrated Security=True;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
